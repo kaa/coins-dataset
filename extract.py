dbfaaa6c0d3d0677547039087055f03fb7c2ab64
Joakim Beijar <jbeijar@gmail.com>
2018-08-24 22:54:33 +0300

    Add more examples

--- extract.py
+++ extract.py
@@ -19,7 +19,8 @@ def loadImage(src):
     return img
 
 def findCoins(img, showCoins = False):
-    scaling = 800/max(img.shape[0:2])
+    scaling = 800.0/max(img.shape[0:2])
+    print scaling
     img_gray = cv2.resize(img, None, fx=scaling, fy=scaling)
     img_gray = cv2.cvtColor(img_gray, cv2.COLOR_BGR2GRAY)
     img_gray = cv2.blur(img_gray, (5,5))
@@ -29,11 +30,12 @@ def findCoins(img, showCoins = False):
 
 if hasattr(main, '__file__'):
     parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument("glob", help = "a glob pattern of source images to process")
-    parser.add_argument("-o", "--output", default="classified", help = "output directory")
+    parser.add_argument("glob", help="a glob pattern of source images to process")
+    parser.add_argument("-o", "--output", default="classified", help="output directory")
     parser.add_argument("-v", "--verbose", action = "store_true")
-    parser.add_argument("--format", default="jpg", choices=["png","jpg"], help = "output format")
-    parser.add_argument("--size", default="150", type=int, help = "dimension of output images")
+    parser.add_argument("--format", default="jpg", choices=["png","jpg"], help="output format")
+    parser.add_argument("--size", default="150", type=int, help="dimension of output images")
+    parser.add_argument("--slice-size", default=None, type=int, help="size of slice in source image")
     args = parser.parse_args()
 
     for srcImage in glob.glob(args.glob):
@@ -49,9 +51,11 @@ if hasattr(main, '__file__'):
         if coins is None:
             break
 
-        maxRadius = np.amax(coins,0)[2]
+        maxRadius = args.slice_size/2 if args.slice_size else np.amax(coins,0)[2]
         for ix,(x,y,r) in enumerate(coins):
             img_coin = img[y-maxRadius:y+maxRadius, x-maxRadius:x+maxRadius]
+            if img_coin.shape[0]==0 or img_coin.shape[1]==0:
+                continue
             img_coin = cv2.resize(img_coin, (args.size,args.size))
             outDir = os.path.join(args.output, typeName)
             if not os.path.exists(outDir):
